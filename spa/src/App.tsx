e0a4443a03be5c978fd5a41e4a1a56ff7f5981ef
Erik Williamson <me@erik.tw>
2019-06-18 23:10:38 -0400

    lint

--- spa/src/App.tsx
+++ spa/src/App.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {FetchTest} from './components/FetchTest'
+import { FetchTest } from './components/FetchTest'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Router>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <Route path="/houses" component={FetchTest} />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <Route path="/houses" component={FetchTest} />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
+        </header>
       </Router>
     </div>
   );
