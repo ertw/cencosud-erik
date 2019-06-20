0121c27adf7f96289fbcdd3e0e366d64a52171c0
Erik Williamson <me@erik.tw>
2019-06-19 22:56:19 -0400

    cleanup

--- spa/src/App.tsx
+++ spa/src/App.tsx
@@ -3,15 +3,13 @@ import './App.css';
 import FetchTest from './components/FetchTest'
 import { BrowserRouter as Router } from "react-router-dom";
 import { Layout } from 'antd';
-const { Header, Footer } = Layout;
+const { Footer } = Layout;
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Router>
         <Layout>
-          <Header>
-          </Header>
             <FetchTest>
             </FetchTest>
           <Footer>
