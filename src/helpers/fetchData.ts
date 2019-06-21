ee4aed1b17e2f9f162398d893743fdd3f5c75816
Erik Williamson <me@erik.tw>
2019-06-21 15:13:17 -0400

    add helper to throw if !reponse.ok because fetch() only throws on network errors

--- src/helpers/fetchData.ts
+++ src/helpers/fetchData.ts
@@ -1,28 +1,37 @@
 import {Houses, Characters, State as DataRequestWrapperState} from '../components/AppStateWrapper'
 
 export const endpoint = 'https://anapioficeandfire.com/api'
+
+const fetchAndThrowOnError = async (request: string) => {
+    const response = await fetch(request)
+    if (response.ok) {
+        return (response)
+    }
+    throw Error(response.statusText)
+}
+
 export const fetchData = async () : Promise<DataRequestWrapperState> => {
 const pageSize = 50
     try {
         let housePageNumber = 1
         let houses: Houses = []
-        let houseResponse = await fetch(`${endpoint}/houses?page=${housePageNumber}&pageSize=${pageSize}`)
+        let houseResponse = await fetchAndThrowOnError(`${endpoint}/houses?page=${housePageNumber}&pageSize=${pageSize}`)
         let returnedHouses = await houseResponse.json()
         houses = houses.concat(returnedHouses)
         while (returnedHouses.length === pageSize) {
             ++housePageNumber
-            houseResponse = await fetch(`${endpoint}/houses?page=${housePageNumber}&pageSize=${pageSize}`)
+            houseResponse = await fetchAndThrowOnError(`${endpoint}/houses?page=${housePageNumber}&pageSize=${pageSize}`)
             returnedHouses = await houseResponse.json()
             houses = houses.concat(returnedHouses)
         }
         let characterPageNumber = 1
         let characters: Characters = []
-        let characterResponse = await fetch(`${endpoint}/characters?page=${characterPageNumber}&pageSize=${pageSize}`)
+        let characterResponse = await fetchAndThrowOnError(`${endpoint}/characters?page=${characterPageNumber}&pageSize=${pageSize}`)
         let returnedCharacters = await characterResponse.json()
         characters = characters.concat(returnedCharacters)
         while (returnedCharacters.length === pageSize) {
             ++characterPageNumber
-            characterResponse = await fetch(`${endpoint}/characters?page=${characterPageNumber}&pageSize=${pageSize}`)
+            characterResponse = await fetchAndThrowOnError(`${endpoint}/characters?page=${characterPageNumber}&pageSize=${pageSize}`)
             returnedCharacters = await characterResponse.json()
             characters = characters.concat(returnedCharacters)
         }
